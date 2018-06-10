d2a400e3a8a1f5a5e24e84d3afa10680bf51b9f2
unknown <rzhan016@ucr.edu>
2018-06-09 17:17:21 -0700

    made item selector more consistent + rotation

--- Assets/Scripts/itemSelector.cs
+++ Assets/Scripts/itemSelector.cs
@@ -5,55 +5,65 @@ using UnityEngine;
 public class itemSelector : MonoBehaviour {
     public float rotationSpeed;
     LineRenderer line;
-    float playerObjDist;
     bool grabbedItem;
 
     // Use this for initialization
     void Start () {
         line = gameObject.GetComponent<LineRenderer>();
-        playerObjDist = 0;
         grabbedItem = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        line.SetPosition(0, transform.position + new Vector3(0.15f, -0.15f, 0)) ;
+        line.SetPosition(0, transform.position) ;
         RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, LayerMask.GetMask("Item")))
         {
             line.SetPosition(1, hit.point);
-            if (Input.GetKeyDown(KeyCode.C))
+            if (Input.GetKeyDown(KeyCode.C) && !grabbedItem)
             {
-                playerObjDist = Vector3.Distance(transform.position,hit.transform.position);
-                grabbedItem = !grabbedItem;
+                //hit.transform.position = new Vector3(transform.position.x, transform.position.y, hit.transform.position.z);
+                hit.transform.position = transform.position + transform.TransformDirection(Vector3.forward) * Vector3.Distance(transform.position, hit.transform.position);
+                hit.transform.parent = transform;
+                grabbedItem = true;
             }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.C) && grabbedItem)
+                {
+                    hit.transform.parent = null;
+                    grabbedItem = false;
+                }
+            }
+
             if (grabbedItem)
             {
                 var d = Input.GetAxis("Mouse ScrollWheel");
                 if (d > 0f)
                 {
-                    playerObjDist += 1;
+                    hit.transform.position += transform.TransformDirection(Vector3.forward);
                 }
                 else if (d < 0f)
                 {
-                    if (playerObjDist > 1)
-                        playerObjDist -= 1;
+                    if(Vector3.Distance(transform.position, hit.transform.position) > 2)
+                        hit.transform.position -= transform.TransformDirection(Vector3.forward);
                 }
-                hit.transform.position = transform.position + transform.TransformDirection(Vector3.forward) * playerObjDist;
-                hit.transform.rotation = transform.rotation;
                 //if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp) || Input.GetKey(KeyCode.I))
+                //{
                 //    hit.transform.Rotate(Vector3.left * Time.deltaTime * rotationSpeed);
-                //if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown) || Input.GetKey(KeyCode.K))
-                //    hit.transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed);
-                //if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft) || Input.GetKey(KeyCode.J))
-                //    hit.transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed);
+                //    Debug.Log("TURN");
+                //}
+                if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown) || Input.GetKey(KeyCode.I))
+                    hit.transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed);
+                if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft) || Input.GetKey(KeyCode.K))
+                    hit.transform.Rotate(Vector3.left * Time.deltaTime * rotationSpeed);
                 //if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight) || Input.GetKey(KeyCode.L))
                 //    hit.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
-                //if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft) || Input.GetKey(KeyCode.LeftArrow))
-                //    hit.transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
-                //if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight) || Input.GetKey(KeyCode.RightArrow))
-                //    hit.transform.Rotate(Vector3.back * Time.deltaTime * rotationSpeed);
+                if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft) || Input.GetKey(KeyCode.J))
+                    hit.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+                if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight) || Input.GetKey(KeyCode.L))
+                    hit.transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed);
             }
         }
         else
