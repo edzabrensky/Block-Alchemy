47b92a2483b4648e087de7984cb0190ebdc42f7b
Ruifeng Zhang <rzhan016@ucr.edu>
2018-06-11 16:38:08 -0700

    updating snapping

--- Assets/Scripts/Snapping.cs
+++ Assets/Scripts/Snapping.cs
@@ -11,11 +11,13 @@ public class Snapping : MonoBehaviour
     bool found = false;
     Vector3 center;
     public bool JointUsed = false;
-    // Use this for initialization
-    void Start()
-    {
 
-    }
+
+    List<Vector3> validRotation;
+    enum cubeStates { corner1, corner2, corner3, corner4 };
+    cubeStates cubeState;
+
+
     void OnTriggerEnter(Collider other)
     {
         other.gameObject.GetComponent<SphereCollider>().isTrigger = false;
@@ -53,7 +55,7 @@ public class Snapping : MonoBehaviour
                 //parent.transform.position = new Vector3(0, 0, 0);
 
                 obj.transform.parent.transform.SetParent(parent.transform);
-                Debug.Log(other.gameObject.name);
+                Debug.Log(other.gameObject.transform.parent.name);
                 transform.root.SetParent(parent.transform);
                 //center = obj.transform.position;
                 //transform.parent.transform.SetPositionAndRotation(center + new Vector3(0f, 0f, -0.5f), obj.transform.rotation);
@@ -61,13 +63,54 @@ public class Snapping : MonoBehaviour
             JointUsed = true;
         }
     }
+
+    Vector3 validPos()
+    {
+        List<Vector3> validPos;
+        for (int i = 0; i < 6; ++i)
+        {
+            if (i < 2)
+            {
+                if(i % 2== 0)
+                {
+                    validPos.Insert(transform.position + new Vector3()
+                }
+                else
+                {
+
+                }
+                
+            }
+            else if (i < 4)
+            {
+                if (i % 2 == 0)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else if(i < 6)
+            {
+                if (i % 2 == 0)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+    }
     // Update is called once per frame
     void Update()
     {
         if(found && JointUsed)
         {
             center = transform.position;
-            obj.transform.parent.transform.SetPositionAndRotation(center + new Vector3(0f,0f,-0.5f), obj.transform.rotation);
+            obj.transform.parent.transform.SetPositionAndRotation(center + new Vector3(0f,0f,-.5f), obj.transform.rotation);
         }
     }
 }
