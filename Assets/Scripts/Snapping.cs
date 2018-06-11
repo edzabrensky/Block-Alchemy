a1acdf6ef212826b2f324da1d09ddaf88ee2f4a5
unknown <ezabr001@ucr.edu>
2018-06-10 19:12:02 -0700

    created snapping with sphere colliders instead of hinges(sphere/box finished) in prefab

--- Assets/Scripts/Snapping.cs
+++ Assets/Scripts/Snapping.cs
@@ -2,27 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Snapping : MonoBehaviour {
-    public SphereCollider sc;
+public class Snapping : MonoBehaviour
+{
     bool attach = true;
+    GameObject obj;
+    GameObject parent;
+    Vector3 offset;
+    bool found = false;
+    Vector3 center;
+    bool JointUsed = false;
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start()
+    {
+
+    }
     void OnTriggerEnter(Collider other)
     {
-        if(attach)
+        if (attach && !JointUsed && !other.gameObject.GetComponent<Snapping>().JointUsed)
         {
-            HingeJoint hinge = gameObject.AddComponent<HingeJoint>();
-            hinge.connectedBody = other.gameObject.GetComponent<Rigidbody>();
-            hinge.anchor = transform.localPosition;
-            //hinge.axis = new Vector3(0f, 1f, 0f);
-            Debug.Log(other.gameObject.name);
+            if(other.gameObject.transform.parent.transform.parent != null)
+            {
+                //transform.position = other.bounds.center;
+                obj = other.gameObject.transform.root.gameObject;
+                parent = obj.transform.root.gameObject;
+                //center = other.transform.position;
+
+
+                obj.transform.parent.transform.SetParent(parent.transform);
+                Debug.Log(other.gameObject.name);
+                transform.root.SetParent(parent.transform);
+            }
+            else
+            {
+                obj = other.gameObject;
+                center = other.bounds.center;
+                //center = other.gameObject.transform.position;
+                Debug.Log(center);
+                found = true;
+
+                //obj.transform.root.transform.SetPositionAndRotation(center, obj.transform.rotation);
+                parent = new GameObject("Combined Obj");
+                //parent.transform.position = new Vector3(0, 0, 0);
+
+                obj.transform.parent.transform.SetParent(parent.transform);
+                Debug.Log(other.gameObject.name);
+                transform.root.SetParent(parent.transform);
+        
+            }
+            JointUsed = true;
         }
         Debug.Log("entered!");
     }
+
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+        if(found)
+        {
+            center = transform.position;
+            obj.transform.parent.transform.SetPositionAndRotation(center + new Vector3(0f,0f,-0.5f), obj.transform.rotation);
+        }
+    }
 }
