6d93fa1f7bf12438c79e0ab4db8e9d7ad7b64d8c
JBeto <jbeto001@ucr.edu>
2018-06-12 01:45:59 -0700

    Made fsm for raycast

--- Assets/Scripts/itemSelector.cs
+++ Assets/Scripts/itemSelector.cs
@@ -1,127 +1,147 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MonsterLove.StateMachine;
 
+// Attach this to the hand
+[RequireComponent(typeof(LineRenderer))]
 public class ItemSelector : MonoBehaviour {
     public float rotationSpeed;
-    LineRenderer line;
-    bool grabbedItem;
+    private LineRenderer line;
     public OVRPlayerController player;
-    private RaycastHit hit;
-    enum RaycastStates { Initial, RaycastActive, RaycastInactive, grabbedObject, releasedObject };
-    private RaycastStates states;
+    private Transform grabbedObject;
 
-    // Use this for initialization
-    void Start () {
-        line = gameObject.GetComponent<LineRenderer>();
-        grabbedItem = false;
-        states = RaycastStates.Initial;
+    private enum RaycastStates { Idle, Raycast, GrabbedObject };
+    private StateMachine<RaycastStates> fsm;
+
+    private void Awake()
+    {
+        this.line = GetComponent<LineRenderer>();
+        this.fsm = StateMachine<RaycastStates>.Initialize(this);
+        this.fsm.ChangeState(RaycastStates.Idle);
+    }
+    
+    private void Idle_Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            fsm.ChangeState(RaycastStates.Raycast);
+        }
     }
 
-    // Update is called once per frame
-
-  void Update () {
-        line.SetPosition(0, transform.position);
-        //switch (states)
-        //{
-        //    case RaycastStates.RaycastInactive:
-        //        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
-        //            states = RaycastStates.RaycastActive;
-        //        break;
-        //    case RaycastStates.RaycastActive:
-        //        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
-        //            states = RaycastStates.RaycastInactive;
-        //        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit,
-        //            5000, LayerMask.GetMask("Selected")))
-        //        {
-        //            line.SetPosition(1, hit.point);
-        //            if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
-        //            {
-        //                states = RaycastStates.grabbedObject;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            line.SetPosition(1, transform.TransformDirection(Vector3.forward) * 5000);
-        //        }
-        //        break;
-        //    case RaycastStates.grabbedObject:
-        //        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
-        //        {
-        //            hit.transform.parent = null;
-        //            player.EnableRotation = true;
-        //            states = RaycastStates.releasedObject;
-        //        }
-        //        break;
-        //}
-
-        //switch (states)
-        //{
-        //    case RaycastStates.RaycastInactive:
-        //        line.SetPosition(1, transform.position);
-        //        break;
-        //    case RaycastStates.RaycastActive:
-        //        ;
-        //        break;
-        //    case RaycastStates.hitObject:
-        //        break;
-        //    case RaycastStates.grabbedObject:
-        //        hit.transform.position = transform.position + transform.TransformDirection(Vector3.forward) * Vector3.Distance(transform.position, hit.transform.position);
-        //        hit.transform.parent = transform;
-        //        //grabbedItem = true;
-        //        player.EnableRotation = false;
-        //        break;
-        //}
-
-        // Does the ray intersect any objects excluding the player layer
-
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, LayerMask.GetMask("Selected")))
+    private void Raycast_Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
         {
-            line.SetPosition(1, hit.point);
-            if (Input.GetKeyDown(KeyCode.C) || OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) && !grabbedItem)
-            {
-                hit.transform.position = transform.position + transform.TransformDirection(Vector3.forward) * Vector3.Distance(transform.position, hit.transform.position);
-                hit.transform.parent = transform;
-                grabbedItem = true;
-                player.EnableRotation = false;
-            }
-            else
-            {
-                if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) && grabbedItem)
-                {
-                    hit.transform.parent = null;
-                    grabbedItem = false;
-                    player.EnableRotation = true;
-                }
-            }
-
-            if (grabbedItem)
-            {
-                var d = Input.GetAxis("Mouse ScrollWheel");
-                if (d > 0f)
-                {
-                    hit.transform.position += transform.TransformDirection(Vector3.forward);
-                }
-                else if (d < 0f)
-                {
-                    if(Vector3.Distance(transform.position, hit.transform.position) > 2)
-                        hit.transform.position -= transform.TransformDirection(Vector3.forward);
-                }
-
-                if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp) || Input.GetKey(KeyCode.I))
-                    hit.transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed);
-                if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown) || Input.GetKey(KeyCode.K))
-                    hit.transform.Rotate(Vector3.left * Time.deltaTime * rotationSpeed);
-                if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft) || Input.GetKey(KeyCode.J))
-                    hit.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
-                if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight) || Input.GetKey(KeyCode.L))
-                    hit.transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed);
-            }
+            fsm.ChangeState(RaycastStates.Idle);
         }
         else
         {
-            line.SetPosition(1, transform.TransformDirection(Vector3.forward) * 5000);
+            RaycastHit hit;
+            this.line.SetPosition(0, transform.position);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit,
+                5000, LayerMask.GetMask("selected")))
+            {
+                this.line.SetPosition(1, hit.point);
+                if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+                {
+                    this.grabbedObject = hit.transform;
+                    fsm.ChangeState(RaycastStates.GrabbedObject);
+                }
+            }
+            else
+            {
+                this.line.SetPosition(1, transform.TransformDirection(Vector3.forward) * 5000);
+            }
         }
-        
+    }
+
+    private void GrabbedObject_Enter()
+    {
+        this.grabbedObject.parent = transform;
+        player.EnableRotation = false;
+    }
+
+    private void GrabbedObject_Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            fsm.ChangeState(RaycastStates.Raycast);
+        }
+        // TODO: Position
+        this.grabbedObject.position = transform.position + transform.TransformDirection(Vector3.forward) * 
+            Vector3.Distance(transform.position, this.grabbedObject.position);
+        // Rotation
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp) || Input.GetKey(KeyCode.I))
+            this.grabbedObject.Rotate(Vector3.right * Time.deltaTime * rotationSpeed);
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown) || Input.GetKey(KeyCode.K))
+            this.grabbedObject.Rotate(Vector3.left * Time.deltaTime * rotationSpeed);
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft) || Input.GetKey(KeyCode.J))
+            this.grabbedObject.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight) || Input.GetKey(KeyCode.L))
+            this.grabbedObject.Rotate(Vector3.down * Time.deltaTime * rotationSpeed);
+        // Sets the line
+        this.line.SetPosition(0, transform.position);
+        this.line.SetPosition(1, this.grabbedObject.position);
+    }
+
+    private void GrabbedObject_Exit()
+    {
+        this.grabbedObject.parent = null;
+        player.EnableRotation = true;
+        this.grabbedObject = null;
     }
 }
+
+// Update is called once per frame
+/*
+void Update () {
+    line.SetPosition(0, transform.position);
+    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, LayerMask.GetMask("Selected")))
+    {
+        line.SetPosition(1, hit.point);
+        if (Input.GetKeyDown(KeyCode.C) || OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) && !grabbedItem)
+        {
+            hit.transform.position = transform.position + transform.TransformDirection(Vector3.forward) * Vector3.Distance(transform.position, hit.transform.position);
+            hit.transform.parent = transform;
+            grabbedItem = true;
+            player.EnableRotation = false;
+        }
+        else
+        {
+            if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) && grabbedItem)
+            {
+                hit.transform.parent = null;
+                grabbedItem = false;
+                player.EnableRotation = true;
+            }
+        }
+
+        if (grabbedItem)
+        {
+            var d = Input.GetAxis("Mouse ScrollWheel");
+            if (d > 0f)
+            {
+                hit.transform.position += transform.TransformDirection(Vector3.forward);
+            }
+            else if (d < 0f)
+            {
+                if(Vector3.Distance(transform.position, hit.transform.position) > 2)
+                    hit.transform.position -= transform.TransformDirection(Vector3.forward);
+            }
+
+            if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp) || Input.GetKey(KeyCode.I))
+                hit.transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed);
+            if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown) || Input.GetKey(KeyCode.K))
+                hit.transform.Rotate(Vector3.left * Time.deltaTime * rotationSpeed);
+            if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft) || Input.GetKey(KeyCode.J))
+                hit.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+            if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight) || Input.GetKey(KeyCode.L))
+                hit.transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed);
+        }
+    }
+    else
+    {
+        line.SetPosition(1, transform.TransformDirection(Vector3.forward) * 5000);
+    }
+}*/
