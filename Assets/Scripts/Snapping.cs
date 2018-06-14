fbde95d0a16378aa1df1bba5f11e6684b277dbba
unknown <ezabr001@ucr.edu>
2018-06-13 17:00:36 -0700

    updated snapping to find more valid positions

--- Assets/Scripts/Snapping.cs
+++ Assets/Scripts/Snapping.cs
@@ -70,99 +70,25 @@ public class Snapping : MonoBehaviour
     {
         List<Vector3> validPos = new List<Vector3>();
         Vector3 min = new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
-        for (int i = 0; i < 14; ++i)
+        Vector3 temp = new Vector3();
+        float SizeOfRenderer = obj.transform.parent.GetComponent<Renderer>().bounds.size.x / 2 > obj.transform.parent.GetComponent<Renderer>().bounds.size.y / 2 ? obj.transform.parent.GetComponent<Renderer>().bounds.size.x / 2 : obj.transform.parent.GetComponent<Renderer>().bounds.size.y / 2;
+        for (float i = -SizeOfRenderer; i <= SizeOfRenderer; i += SizeOfRenderer)
         {
-            Vector3 x = new Vector3();
-            float SizeOfRenderer = obj.transform.parent.GetComponent<Renderer>().bounds.size.x / 2 > obj.transform.parent.GetComponent<Renderer>().bounds.size.y / 2 ? obj.transform.parent.GetComponent<Renderer>().bounds.size.x / 2 : obj.transform.parent.GetComponent<Renderer>().bounds.size.y / 2;
-            //float SizeOfRenderer = obj.transform.parent.GetComponent<Renderer>().bounds.size.y / 2;
-            if (i < 2)
+            for (float j = -SizeOfRenderer; j <= SizeOfRenderer; j += SizeOfRenderer)
             {
-                
-                if (i % 2 == 0)
+                for (float k = -SizeOfRenderer; k <= SizeOfRenderer; k += SizeOfRenderer)
                 {
-                    x = transform.position + new Vector3(SizeOfRenderer, 0f, 0f);
-                }
-                else
-                {
-                    x = transform.position - new Vector3(SizeOfRenderer, 0f, 0f);
-                }
-
-            }
-            else if (i < 4)
-            {
-                if (i % 2 == 0)
-                {
-                    x = transform.position + new Vector3(0f, SizeOfRenderer, 0f);
-                }
-                else
-                {
-                    x = transform.position - new Vector3(0f, SizeOfRenderer, 0f);
+                    temp = transform.position + new Vector3(i, j, k);
+                    if (!transform.parent.GetComponent<Collider>().bounds.Contains(temp))
+                    {
+                        validPos.Add(temp);
+                        if (Vector3.Distance(obj.transform.parent.position, min) > Vector3.Distance(obj.transform.parent.position, temp) && Vector3.Distance(obj.transform.position, min) > Vector3.Distance(obj.transform.position, temp))
+                        {
+                            min = temp;
+                        }
+                    }
                 }
             }
-            else if (i < 6)
-            {
-                if (i % 2 == 0)
-                {
-                    x = transform.position + new Vector3(0f, 0f, SizeOfRenderer);
-                }
-                else
-                {
-                    x = transform.position - new Vector3(0f, 0f, SizeOfRenderer);
-                }
-            }
-            else if(i < 8)
-            {
-                if (i % 2 == 0)
-                {
-                    x = transform.position + new Vector3(SizeOfRenderer, SizeOfRenderer, 0f);
-                }
-                else
-                {
-                    x = transform.position - new Vector3(SizeOfRenderer, SizeOfRenderer, 0f);
-                }
-            }
-            else if(i < 10)
-            {
-                if (i % 2 == 0)
-                {
-                    x = transform.position + new Vector3(SizeOfRenderer, 0f, SizeOfRenderer);
-                }
-                else
-                {
-                    x = transform.position - new Vector3(SizeOfRenderer, 0f, SizeOfRenderer);
-                }
-            }
-            else if(i < 12)
-            {
-                if (i % 2 == 0)
-                {
-                    x = transform.position + new Vector3(0f, SizeOfRenderer, SizeOfRenderer);
-                }
-                else
-                {
-                    x = transform.position - new Vector3(0f, SizeOfRenderer, SizeOfRenderer);
-                }
-            }
-            else if(i < 14)
-            {
-                if (i % 2 == 0)
-                {
-                    x = transform.position + new Vector3(SizeOfRenderer, SizeOfRenderer, SizeOfRenderer);
-                }
-                else
-                {
-                    x = transform.position - new Vector3(SizeOfRenderer, SizeOfRenderer, SizeOfRenderer);
-                }
-            }
-            if(!transform.parent.GetComponent<Collider>().bounds.Contains(x))
-            {
-                validPos.Add(x);
-                if(Vector3.Distance(obj.transform.parent.position, min) > Vector3.Distance(obj.transform.parent.position, x) && Vector3.Distance(obj.transform.position, min) > Vector3.Distance(obj.transform.position, x))
-                {
-                    min = x;
-                }
-            }
-            //Debug.Log(obj.transform.parent.GetComponent<Collider>().bounds.size.y * 2);
         }
         dist = transform.parent.position - min;
         return min;
